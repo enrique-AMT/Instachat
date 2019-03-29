1c0281649447643703e36de4a29adbbf450ea059
enrique <enrique.marrero1@upr.edu>
2019-03-29 09:24:06 -0400

    converted schema dump to complete database dump

--- schema.sql
+++ schema.sql
@@ -5,8 +5,6 @@
 -- Dumped from database version 10.6 (Ubuntu 10.6-0ubuntu0.18.04.1)
 -- Dumped by pg_dump version 10.6 (Ubuntu 10.6-0ubuntu0.18.04.1)
 
--- Started on 2019-03-28 22:25:36 AST
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -18,7 +16,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 7 (class 2615 OID 24594)
 -- Name: instachat; Type: SCHEMA; Schema: -; Owner: instadev
 --
 
@@ -27,12 +24,25 @@ CREATE SCHEMA instachat;
 
 ALTER SCHEMA instachat OWNER TO instadev;
 
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- TOC entry 206 (class 1259 OID 24646)
 -- Name: belongs; Type: TABLE; Schema: instachat; Owner: instadev
 --
 
@@ -45,7 +55,6 @@ CREATE TABLE instachat.belongs (
 ALTER TABLE instachat.belongs OWNER TO instadev;
 
 --
--- TOC entry 205 (class 1259 OID 24635)
 -- Name: chat; Type: TABLE; Schema: instachat; Owner: instadev
 --
 
@@ -60,7 +69,6 @@ CREATE TABLE instachat.chat (
 ALTER TABLE instachat.chat OWNER TO instadev;
 
 --
--- TOC entry 204 (class 1259 OID 24633)
 -- Name: chat_chat_id_seq; Type: SEQUENCE; Schema: instachat; Owner: instadev
 --
 
@@ -76,8 +84,6 @@ CREATE SEQUENCE instachat.chat_chat_id_seq
 ALTER TABLE instachat.chat_chat_id_seq OWNER TO instadev;
 
 --
--- TOC entry 2965 (class 0 OID 0)
--- Dependencies: 204
 -- Name: chat_chat_id_seq; Type: SEQUENCE OWNED BY; Schema: instachat; Owner: instadev
 --
 
@@ -85,7 +91,6 @@ ALTER SEQUENCE instachat.chat_chat_id_seq OWNED BY instachat.chat.chat_id;
 
 
 --
--- TOC entry 202 (class 1259 OID 24607)
 -- Name: phone; Type: TABLE; Schema: instachat; Owner: instadev
 --
 
@@ -99,7 +104,6 @@ CREATE TABLE instachat.phone (
 ALTER TABLE instachat.phone OWNER TO instadev;
 
 --
--- TOC entry 201 (class 1259 OID 24605)
 -- Name: phone_phone_id_seq; Type: SEQUENCE; Schema: instachat; Owner: instadev
 --
 
@@ -115,8 +119,6 @@ CREATE SEQUENCE instachat.phone_phone_id_seq
 ALTER TABLE instachat.phone_phone_id_seq OWNER TO instadev;
 
 --
--- TOC entry 2966 (class 0 OID 0)
--- Dependencies: 201
 -- Name: phone_phone_id_seq; Type: SEQUENCE OWNED BY; Schema: instachat; Owner: instadev
 --
 
@@ -124,7 +126,6 @@ ALTER SEQUENCE instachat.phone_phone_id_seq OWNED BY instachat.phone.phone_id;
 
 
 --
--- TOC entry 203 (class 1259 OID 24618)
 -- Name: u_contacts; Type: TABLE; Schema: instachat; Owner: instadev
 --
 
@@ -137,7 +138,6 @@ CREATE TABLE instachat.u_contacts (
 ALTER TABLE instachat.u_contacts OWNER TO instadev;
 
 --
--- TOC entry 200 (class 1259 OID 24597)
 -- Name: user; Type: TABLE; Schema: instachat; Owner: instadev
 --
 
@@ -153,7 +153,6 @@ CREATE TABLE instachat."user" (
 ALTER TABLE instachat."user" OWNER TO instadev;
 
 --
--- TOC entry 199 (class 1259 OID 24595)
 -- Name: user_user_id_seq; Type: SEQUENCE; Schema: instachat; Owner: instadev
 --
 
@@ -169,8 +168,6 @@ CREATE SEQUENCE instachat.user_user_id_seq
 ALTER TABLE instachat.user_user_id_seq OWNER TO instadev;
 
 --
--- TOC entry 2967 (class 0 OID 0)
--- Dependencies: 199
 -- Name: user_user_id_seq; Type: SEQUENCE OWNED BY; Schema: instachat; Owner: instadev
 --
 
@@ -178,7 +175,44 @@ ALTER SEQUENCE instachat.user_user_id_seq OWNED BY instachat."user".user_id;
 
 
 --
--- TOC entry 2812 (class 2604 OID 24638)
+-- Name: chats; Type: TABLE; Schema: public; Owner: instadev
+--
+
+CREATE TABLE public.chats (
+    chat_id integer NOT NULL,
+    chat_name character varying(20),
+    number_of_users integer,
+    user_id character varying(200),
+    active_user_count integer,
+    owner_id integer
+);
+
+
+ALTER TABLE public.chats OWNER TO instadev;
+
+--
+-- Name: chats_chat_id_seq; Type: SEQUENCE; Schema: public; Owner: instadev
+--
+
+CREATE SEQUENCE public.chats_chat_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.chats_chat_id_seq OWNER TO instadev;
+
+--
+-- Name: chats_chat_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: instadev
+--
+
+ALTER SEQUENCE public.chats_chat_id_seq OWNED BY public.chats.chat_id;
+
+
+--
 -- Name: chat chat_id; Type: DEFAULT; Schema: instachat; Owner: instadev
 --
 
@@ -186,7 +220,6 @@ ALTER TABLE ONLY instachat.chat ALTER COLUMN chat_id SET DEFAULT nextval('instac
 
 
 --
--- TOC entry 2811 (class 2604 OID 24610)
 -- Name: phone phone_id; Type: DEFAULT; Schema: instachat; Owner: instadev
 --
 
@@ -194,7 +227,6 @@ ALTER TABLE ONLY instachat.phone ALTER COLUMN phone_id SET DEFAULT nextval('inst
 
 
 --
--- TOC entry 2810 (class 2604 OID 24600)
 -- Name: user user_id; Type: DEFAULT; Schema: instachat; Owner: instadev
 --
 
@@ -202,8 +234,13 @@ ALTER TABLE ONLY instachat."user" ALTER COLUMN user_id SET DEFAULT nextval('inst
 
 
 --
--- TOC entry 2959 (class 0 OID 24646)
--- Dependencies: 206
+-- Name: chats chat_id; Type: DEFAULT; Schema: public; Owner: instadev
+--
+
+ALTER TABLE ONLY public.chats ALTER COLUMN chat_id SET DEFAULT nextval('public.chats_chat_id_seq'::regclass);
+
+
+--
 -- Data for Name: belongs; Type: TABLE DATA; Schema: instachat; Owner: instadev
 --
 
@@ -212,8 +249,6 @@ COPY instachat.belongs (chat_id, user_id) FROM stdin;
 
 
 --
--- TOC entry 2958 (class 0 OID 24635)
--- Dependencies: 205
 -- Data for Name: chat; Type: TABLE DATA; Schema: instachat; Owner: instadev
 --
 
@@ -223,8 +258,6 @@ COPY instachat.chat (chat_id, chat_name, owner_id, number_of_users) FROM stdin;
 
 
 --
--- TOC entry 2955 (class 0 OID 24607)
--- Dependencies: 202
 -- Data for Name: phone; Type: TABLE DATA; Schema: instachat; Owner: instadev
 --
 
@@ -234,8 +267,6 @@ COPY instachat.phone (phone_id, user_id, phone) FROM stdin;
 
 
 --
--- TOC entry 2956 (class 0 OID 24618)
--- Dependencies: 203
 -- Data for Name: u_contacts; Type: TABLE DATA; Schema: instachat; Owner: instadev
 --
 
@@ -245,8 +276,6 @@ COPY instachat.u_contacts (user_id, contact_id) FROM stdin;
 
 
 --
--- TOC entry 2953 (class 0 OID 24597)
--- Dependencies: 200
 -- Data for Name: user; Type: TABLE DATA; Schema: instachat; Owner: instadev
 --
 
@@ -259,8 +288,15 @@ COPY instachat."user" (user_id, first_name, last_name, u_email_address, u_passwo
 
 
 --
--- TOC entry 2968 (class 0 OID 0)
--- Dependencies: 204
+-- Data for Name: chats; Type: TABLE DATA; Schema: public; Owner: instadev
+--
+
+COPY public.chats (chat_id, chat_name, number_of_users, user_id, active_user_count, owner_id) FROM stdin;
+1	testing	5	[Pedrito, Manuel, Bienve, Wilson, Amir]	4	283
+\.
+
+
+--
 -- Name: chat_chat_id_seq; Type: SEQUENCE SET; Schema: instachat; Owner: instadev
 --
 
@@ -268,8 +304,6 @@ SELECT pg_catalog.setval('instachat.chat_chat_id_seq', 1, true);
 
 
 --
--- TOC entry 2969 (class 0 OID 0)
--- Dependencies: 201
 -- Name: phone_phone_id_seq; Type: SEQUENCE SET; Schema: instachat; Owner: instadev
 --
 
@@ -277,8 +311,6 @@ SELECT pg_catalog.setval('instachat.phone_phone_id_seq', 1, true);
 
 
 --
--- TOC entry 2970 (class 0 OID 0)
--- Dependencies: 199
 -- Name: user_user_id_seq; Type: SEQUENCE SET; Schema: instachat; Owner: instadev
 --
 
@@ -286,7 +318,13 @@ SELECT pg_catalog.setval('instachat.user_user_id_seq', 3, true);
 
 
 --
--- TOC entry 2824 (class 2606 OID 24650)
+-- Name: chats_chat_id_seq; Type: SEQUENCE SET; Schema: public; Owner: instadev
+--
+
+SELECT pg_catalog.setval('public.chats_chat_id_seq', 1, true);
+
+
+--
 -- Name: belongs belongs_pkey; Type: CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -295,7 +333,6 @@ ALTER TABLE ONLY instachat.belongs
 
 
 --
--- TOC entry 2822 (class 2606 OID 24640)
 -- Name: chat chat_pkey; Type: CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -304,7 +341,6 @@ ALTER TABLE ONLY instachat.chat
 
 
 --
--- TOC entry 2818 (class 2606 OID 24612)
 -- Name: phone phone_pkey; Type: CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -313,7 +349,6 @@ ALTER TABLE ONLY instachat.phone
 
 
 --
--- TOC entry 2820 (class 2606 OID 24622)
 -- Name: u_contacts u_contacts_pkey; Type: CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -322,7 +357,6 @@ ALTER TABLE ONLY instachat.u_contacts
 
 
 --
--- TOC entry 2814 (class 2606 OID 24602)
 -- Name: user user_pkey; Type: CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -331,7 +365,6 @@ ALTER TABLE ONLY instachat."user"
 
 
 --
--- TOC entry 2816 (class 2606 OID 24604)
 -- Name: user user_u_email_address_key; Type: CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -340,7 +373,14 @@ ALTER TABLE ONLY instachat."user"
 
 
 --
--- TOC entry 2829 (class 2606 OID 24651)
+-- Name: chats chats_pkey; Type: CONSTRAINT; Schema: public; Owner: instadev
+--
+
+ALTER TABLE ONLY public.chats
+    ADD CONSTRAINT chats_pkey PRIMARY KEY (chat_id);
+
+
+--
 -- Name: belongs belongs_chat_id_fkey; Type: FK CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -349,7 +389,6 @@ ALTER TABLE ONLY instachat.belongs
 
 
 --
--- TOC entry 2830 (class 2606 OID 24656)
 -- Name: belongs belongs_user_id_fkey; Type: FK CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -358,7 +397,6 @@ ALTER TABLE ONLY instachat.belongs
 
 
 --
--- TOC entry 2828 (class 2606 OID 24641)
 -- Name: chat chat_owner_id_fkey; Type: FK CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -367,7 +405,6 @@ ALTER TABLE ONLY instachat.chat
 
 
 --
--- TOC entry 2825 (class 2606 OID 24613)
 -- Name: phone phone_user_id_fkey; Type: FK CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -376,7 +413,6 @@ ALTER TABLE ONLY instachat.phone
 
 
 --
--- TOC entry 2827 (class 2606 OID 24628)
 -- Name: u_contacts u_contacts_contact_id_fkey; Type: FK CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
@@ -385,15 +421,12 @@ ALTER TABLE ONLY instachat.u_contacts
 
 
 --
--- TOC entry 2826 (class 2606 OID 24623)
 -- Name: u_contacts u_contacts_user_id_fkey; Type: FK CONSTRAINT; Schema: instachat; Owner: instadev
 --
 
 ALTER TABLE ONLY instachat.u_contacts
     ADD CONSTRAINT u_contacts_user_id_fkey FOREIGN KEY (user_id) REFERENCES instachat."user"(user_id);
 
-
--- Completed on 2019-03-28 22:25:36 AST
 
 --
 -- PostgreSQL database dump complete
