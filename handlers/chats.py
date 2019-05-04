7288a4cbeee068038660d7995d7ceb90ecdf0c02
Brian <brian.munoz1@upr.edu>
2019-05-04 02:39:27 -0400

    Test

--- handlers/chats.py
+++ handlers/chats.py
@@ -23,8 +23,8 @@ class ChatHandler:
         chat_list = dao.getAllChats()
         result_list = []
         for row in chat_list:
-          result = self.build_chat_dict(row)
-          result_list.append(result)
+            result = self.build_chat_dict(row)
+            result_list.append(result)
         return jsonify(Chat=result_list)
 
     def getChatById(self, chat_id):
@@ -97,24 +97,17 @@ class ChatHandler:
 
     def updateChat(self, chat_id, json):
       print("todo")
-        # if len(chats_list) < chat_id or chat_id < 1:
-        #     return jsonify(Error = "Chat not found."), 404
-        # else:
-        #     if len(json) != 5:
-        #         return jsonify(Error = "Update request incorrect."), 400
-        #     else:
-        #         chat_name = json['chat_name']
-        #         number_of_users = json['number_of_users']
-        #         user_id = json['user_id']
-        #         active_user_count = json['active_user_count']
-        #         owner_id = json['owner_id']
-        #         if chat_name and number_of_users and user_id and active_user_count and owner_id:
-        #             return jsonify(UpdateStatus = "AREA TO UPDATE CHAT BY ID"), 200
+      # if len(chats_list) < chat_id or chat_id < 1:
+      #     return jsonify(Error = "Chat not found."), 404
+      # else:
+      #     if len(json) != 5:
+      #         return jsonify(Error = "Update request incorrect."), 400
+      #     else:
+      #         chat_name = json['chat_name']
+      #         number_of_users = json['number_of_users']
+      #         user_id = json['user_id']
+      #         active_user_count = json['active_user_count']
+      #         owner_id = json['owner_id']
+      #         if chat_name and number_of_users and user_id and active_user_count and owner_id:
+      #             return jsonify(UpdateStatus = "AREA TO UPDATE CHAT BY ID"), 200
 
-    def deleteChat(self, chat_id):
-      print("todo")
-        # global cid
-        # if len(chats_list) < chat_id or chat_id < 1:
-        #     return jsonify(Error = "Chat not found."), 404
-        # else:
-        #     return jsonify(DeleteStatus = "AREA TO DELETE CHAT BY ID"), 200
