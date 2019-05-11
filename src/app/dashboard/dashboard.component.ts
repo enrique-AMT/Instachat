f9bb7015473b797072f84e2b68e20954e95df3d8
Eduardo <eduardo.nieves1@upr.edu>
2019-05-11 00:21:22 -0400

    Fix

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -28,10 +28,6 @@ export class DashboardHashtag {
 })
 export class DashboardComponent implements OnInit {
 
-  if (localStorage.getItem('user_id') === '' || localStorage.getItem('user_id') === null) {
-  this.router.navigate(['login']);
-}
-
   dataSource = new DashboardPostDataSource(this.server);
   hashtagSource: DashboardHashtagDataSource;
 
@@ -52,6 +48,10 @@ export class DashboardComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
+    if (localStorage.getItem('user_id') === '' || localStorage.getItem('user_id') === null) {
+      this.router.navigate(['login']);
+    }
 
     this.server.getDashboardPosts().subscribe(
       data => {
